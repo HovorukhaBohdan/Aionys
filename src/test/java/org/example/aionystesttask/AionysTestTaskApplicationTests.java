18369996dd5005165cf2abaecbd73098e15b97a1
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-05-31 16:06:42 +0300

    Added infrastructure

---
+++ src/test/java/org/example/aionystesttask/AionysTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aionystesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AionysTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
