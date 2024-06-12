e8b6e4d89ae725a6c837be686759ad2204f05348
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-06-12 19:19:27 +0300

    Created React app

---
+++ back/src/main/java/org/example/aionystesttask/AionysTestTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.aionystesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AionysTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AionysTestTaskApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/org/example/aionystesttask/AionysTestTaskApplicationTests.java
@@ -0,0 +1,8 @@
+package org.example.aionystesttask;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AionysTestTaskApplicationTests {
+
+}
