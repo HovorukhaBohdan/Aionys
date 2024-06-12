02e15678214ae7b38891e90785fb14e57180f449
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-06-12 18:40:40 +0300

    Setted up CORS

--- src/main/java/org/example/aionystesttask/controller/NoteController.java
+++ src/main/java/org/example/aionystesttask/controller/NoteController.java
@@ -7,19 +7,12 @@ import org.example.aionystesttask.dto.NoteResponseDto;
 import org.example.aionystesttask.exception.EntityNotFoundException;
 import org.example.aionystesttask.service.NoteService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notes")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class NoteController {
     private final NoteService noteService;
 
