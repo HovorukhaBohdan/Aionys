bba7d30d776ae35ae9c8b67ccd43ac77afe53771
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-06-12 19:48:14 +0300

    Added components, redux, i18n

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotesList from './components/NoteList';
+import NoteDetail from './components/NoteDetail';
+import NoteForm from './components/NoteForm';
+import LanguageSwitcher from './components/LanguageSwitcher';
 
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <Router>
+            <div>
+                <LanguageSwitcher />
+                <Routes>
+                    <Route path="/" element={<NotesList />} />
+                    <Route path="/notes/new" element={<NoteForm />} />
+                    <Route path="/notes/:id/edit" element={<NoteForm />} />
+                    <Route path="/notes/:id" element={<NoteDetail />} />
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
